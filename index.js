6106419e10c41591a90cb443b3d28137ea558f8a
nilar <nicolas.larche@rca.fr>
2016-11-03 12:01:33 +0100

    add babel

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-import createObservable from './src/createObservable';
-
-const arrayObservable = createObservable(function subscribe(observer) {
-    [10, 20, 30].forEach(x => observer.next(x));
-    observer.complete();
-});
-
-const clickObservable = createObservable(function subscribe(observer) {
-    document.addEventListener('click', observer.next)
-});
-
-const observer = {
-    next: function next(period) { console.log(`next -> ${period}`) },
-    error: function error(err) { console.log(`error -> ${err}`) },
-    complete: function complete() { console.log(`done`) }
-}
-
-arrayObservable
-    .map(x => x / 10)
-    .filter(x => x !== 2)
-    .delay(2000)
-    .subscribe(observer);
+require('babel-register');
+require('./src');
